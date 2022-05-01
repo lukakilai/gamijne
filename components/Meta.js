51568350b5b444214c0d5cb5959bdbdc94f1b6ab
Luka Kilaberidze <lkila16@freeuni.edu.ge>
2022-05-01 10:37:34 +0400

    fix: add thumbnail

--- components/Meta.js
+++ components/Meta.js
@@ -7,12 +7,14 @@ export default function Meta({ title, url, ogtitle }) {
     <Head>
       <title>{title}</title>
       <meta property="og:title" content={ogtitle} />
-
       <meta property="og:description" content={title} />
-      <meta property="og:image" content={thumbnail} />
+      <meta
+        property="og:image"
+        content="https://drive.google.com/file/d/1s5c0AvkWmF0KmrqHCyhH7KZgJL-niWkB/view?usp=sharing"
+      />
       <meta property="og:image:alt" content="mijna" />
-
       <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
     </Head>
   );
 }
